f441ae0d0533cff30d0574f40342b9728e1b3bda
mikolaj-dobosz-wttech <mikolaj.dobosz@wundermanthompson.com>
2023-03-15 17:01:06 +0100

    fix5

--- bentoml/adapters/image_input.py
+++ bentoml/adapters/image_input.py
@@ -225,7 +225,7 @@ class ImageInput(FileInput):
                 logger.warning('task.data')
                 logger.warning(task.data)
                 logger.warning(type(task.data))
-                image_pil = Image.open(io.BytesIO(task.data))
+                image_pil = Image.open(io.BytesIO(task.data.bytes_))
                 # img_array = np.array(image_pil)
                 img_array = imageio.imread(image_pil, pilmode=self.pilmode)
                 #read from binary
